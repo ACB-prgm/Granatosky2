139e6f566eb8a9ad42783ec9ade49c593fc9cb4a
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2022-04-05 20:35:23 -0400

    poo

--- ChameleonData/Chameleon.py
+++ ChameleonData/Chameleon.py
@@ -1,3 +1,4 @@
+from ntpath import join
 import pandas as pd
 import statistics as stats
 import math
@@ -5,20 +6,15 @@ import os
 
 
 STRIDE_INFO_PATH = "ChameleonData/StrideTimes/Spatiotemporalvariables.xlsx"
+stride_info_DF = pd.read_excel(STRIDE_INFO_PATH).fillna("OK")
 OUTPUT = "ChameleonData/Angles_Outputs"
-FPS = 120
+FPS = 120.0
 
-angles = {
+ANGLES = {
     "hindlimb" : ["hip", "knee", "h"],
     "ankle" : ["ankle", "hip", "h"],
     "forelimb" : ["glenoid", "elbow", "h"],
     "wrist" : ["wrist", "elbow", "h"]
-}
-VentSnouts = {"Dopey": 49,
-    "Megan" : 55,
-    "Abby" : 57,
-    "Snow" : 106,
-    "Mia" : 54
 }
 
 SVLs = {
@@ -27,32 +23,59 @@ SVLs = {
     "abby" : 0.057,
     "snow" : 0.106,
     "mia" : 0.054,
-} # in mm
+} # in meters
 
 
 def main():
+    fix_event_mismatches()
+
     for root, dirs, files in os.walk("ChameleonData/Chameleons", topdown=False):
+        TOTAL_FILES = len(files)
+        count = 0
         for file in files:
+            count += 1
             if ".csv" in file:
-                process_csv(os.path.join(root, file), file, root.split("/")[-1].lower())
+                chameleon_name = root.split("/")[-1].lower()
+                process_csv(os.path.join(root, file), file, chameleon_name)
+
+                print("{} PROGRESS... {}/{}".format(chameleon_name.upper(), count, TOTAL_FILES), end="\r")
+        print("")
 
 
 def process_csv(CSV, filename, chameleon):
-    output = {}
-    anim_output = {}
+    output = {
+        "FRAME" : []
+    }
+    # anim_output = {}
     df = pd.read_csv(CSV, header=1)
 
-    print(chameleon)
-    stride_info_DF = pd.read_excel(STRIDE_INFO_PATH)
-    stride_info_DF = stride_info_DF.fillna("OK")
+    ref_file = filename.split("DLC")[0]
+    if ref_file in stride_info_DF.index:
+        strides_df = []
+        for _index, row in stride_info_DF.loc[[ref_file]].iterrows():  # REMOVES ALL VALUES NOT IN STRIDE(S)
+            if row["OK trial?"] == "OK":
+                FRAMES = get_stride_frames(row["a"], row["b"], row["c"])
+                AC = df.iloc[FRAMES[0]:FRAMES[2]]
+                strides_df.append(AC)
+                output["FRAME"] += range(FRAMES[0], FRAMES[2])
+                # print(ref_file, len(AC.index), len(range(FRAMES[0], FRAMES[2])), range(FRAMES[0], FRAMES[2]))
+        if strides_df:
+            strides_df = pd.concat(strides_df) # NOTE: MUST USE .loc INSTEAD OF .iloc AFTER CONCATENATION **************************************
+        else:
+            return
 
-    for index, row in df.iterrows():
-        if not index == 0:
-            for angle in angles:
+        REF = []
+        SUB_Y = []
+
+        for _idx, row in strides_df.iterrows():
+            REF.append(get_distance_between( (row["snout"], row["snout.1"]) , (row["vent"], row["vent.1"]) ))
+            SUB_Y.append((float(row["ankle.1"]) + float(row["wrist.1"])) / 2.0)
+
+            for angle in ANGLES: # ANGLE PROCESSING
                 if not angle in output.keys():
                     output[angle] = []
-                    anim_output[angle] = []
-                angl = angles.get(angle)
+                    # anim_output[angle] = []
+                angl = ANGLES.get(angle)
                 point1 = (row[angl[0]], row[angl[0] + ".1"])
                 point2 = (row[angl[1]], row[angl[1] + ".1"])
                 point3 = None
@@ -61,13 +84,32 @@ def process_csv(CSV, filename, chameleon):
                 elif angl[2] == "h":
                     point3 = (float(row[angl[0]]) - 100.0, row[angl[0] + ".1"])
                 output.get(angle).append(get_angle(point1, point2, point3))
-                anim_output.get(angle).append("{},{}:{},{}:{},{}".format(point2[0], point2[1], point1[0], point1[1], point3[0], point3[1]))
-    
-    anim_output = pd.DataFrame(anim_output)
-    anim_output.to_excel(OUTPUT + "/anim_output.xlsx")
-    output = pd.DataFrame(output)
-    output.to_excel(OUTPUT + "/{}.xlsx".format(filename.split("chameleon")[0]))
-    ankle = [float(x) for x in df.get("ankle.1") if x != "y"]
+                # anim_output.get(angle).append("{},{}:{},{}:{},{}".format(point2[0], point2[1], point1[0], point1[1], point3[0], point3[1]))
+
+        REF = stats.mean(REF) / SVLs.get(chameleon)
+        SUB_Y = stats.median(SUB_Y)
+
+        output["glenoid_height"] = []
+        output["hip_height"] = []
+
+        for _idx, row in strides_df.iterrows():
+            output.get("glenoid_height").append((SUB_Y - float(row["glenoid.1"])) / REF)
+            output.get("hip_height").append((SUB_Y - float(row["hip.1"])) / REF)
+        
+        # anim_output = pd.DataFrame(anim_output)
+        # anim_output.to_excel(OUTPUT + "/anim_output.xlsx")
+        output = pd.DataFrame(output)
+        output.to_excel(OUTPUT + "/{}.xlsx".format(filename.split("chameleon")[0]))
+        # ankle = [float(x) for x in df.get("ankle.1") if x != "y"]
+
+
+def fix_event_mismatches():
+    global stride_info_DF
+    for index, row in stride_info_DF.iterrows():
+        file_name = row["File name"].split("Event_")
+        if str(row["Event"]) != file_name[1]:
+            stride_info_DF.iloc[index, [0]] = file_name[0] + str(row["Event"])
+    stride_info_DF.set_index("File name", inplace=True)
 
 
 def get_angle(P1, P2, P3):
@@ -85,13 +127,27 @@ def get_side(A, B):
     return math.sqrt( (float(A[0]) - float(B[0]))**2 + (float(A[1]) - float(B[1]))**2 )
 
 
+def get_distance_between(P1, P2, REF=None):
+    pixel_dist = math.sqrt( ((float(P1[0]) - float(P2[0])) **2)+((float(P1[1]) - float(P2[1])) **2) )
+    
+    if REF: # RETURNS DISTANCE IN METERS (REF IS pixel/meter)
+        return (pixel_dist / REF)
+    else: # RETURNS DISTANCE IN PIXELS
+        return pixel_dist
+
+
 def get_stride_frames(a, b, c):
     A = round(a * FPS)
     B = round(b * FPS)
-    C = round(c * FPS)
+    C = round(c * FPS) + 1
 
     return [A, B, C]
 
 
 if __name__ == "__main__":
     main()
+
+
+# TODO
+# CLIP TO STRIDE TIMES √
+# calculate hip and shoulder height √
--- ChameleonData/StrideTimes/ChameleonStrides.py
+++ ChameleonData/StrideTimes/ChameleonStrides.py
@@ -1,53 +1,82 @@
-from fileinput import filename
+import statistics as stats
 import pandas as pd
 import os
 
-STRIDE_INFO_PATH = "ChameleonData/StrideTimes/Spatiotemporalvariables.xlsx"
+FPS = 120.0
+LIMBS = {
+    "forelimb" : ["forelimb", "wrist"], 
+    "hindlimb" : ["hindlimb", "ankle"]
+}
 STRIDE_DATA_FILES_PATH = "ChameleonData/Angles_Outputs"
 DLC_EXTENSION = "DLC_resnet50_.xlsx"
 OUTPUT_PATH = "ChameleonData/StrideTimes/StrideOutputs"
-FPS = 120
+STRIDE_INFO_PATH = "ChameleonData/StrideTimes/Spatiotemporalvariables.xlsx"
+stride_info_DFs = pd.read_excel(STRIDE_INFO_PATH, sheet_name=None)
 
 Export_DFs = {}
 
 
 def main():
     # PROCESS THE EXCEL
-    stride_info_DF = pd.read_excel(STRIDE_INFO_PATH)
-    stride_info_DF = stride_info_DF.fillna("OK")
+    for SHEET in stride_info_DFs:
+        if SHEET != "hindlimb":  # REMOVE ########################################### !!
+            continue
+        
+        SHEET_DF = stride_info_DFs.get(SHEET).fillna("OK")
 
-    for index, row in stride_info_DF.iterrows():
-        print("PROCESSING... {}/{}".format(index, len(stride_info_DF.index)), end="\r")
-        if row["OK trial?"] == "OK":
-            FRAMES = get_stride_frames(row["a"], row["b"], row["c"])
-            file_name = row["File name"]
-            file_path = os.path.join(STRIDE_DATA_FILES_PATH, file_name + DLC_EXTENSION)
-            if os.path.exists(file_path):
-                DF = pd.read_excel(file_path).interpolate()
+        for index, row in SHEET_DF.iterrows(): # Fixes all event mismatches
+            file_name = row["File name"].split("Event_")
+            if str(row["Event"]) != file_name[1]:
+                SHEET_DF.iloc[index, [0]] = file_name[0] + str(row["Event"])
+        
+        for index, row in SHEET_DF.iterrows():
+            print("PROCESSING... {} {}/{}".format(SHEET, index, len(SHEET_DF.index)), end="\r")
+            if row["OK trial?"] == "OK":
+                FRAMES = get_stride_frames(row["a"], row["b"], row["c"])
+                file_name = row["File name"]
+                file_path = os.path.join(STRIDE_DATA_FILES_PATH, file_name + DLC_EXTENSION)
+                if os.path.exists(file_path):
+                    DF = pd.read_excel(file_path, index_col="FRAME").interpolate()
 
-                if not file_name in Export_DFs:
-                    Export_DFs[file_name] = {
-                        "AB hindlimb" : {},
-                        "AB ankle" : {},
-                        "AC hindlimb" : {},
-                        "AC ankle" : {}
-                        }
-                
-                AB = DF.iloc[FRAMES[0]:FRAMES[1]]
-                AC = DF.iloc[FRAMES[0]:FRAMES[2]]
-                AC = scale_dataframe(AC)
+                    if not file_name in Export_DFs:
+                        Export_DFs[file_name] = {}
+                        for limb in LIMBS:
+                            for bodypart in LIMBS.get(limb):
+                                Export_DFs[file_name]["AB_{}".format(bodypart)] = {}
+                                Export_DFs[file_name]["AC_{}".format(bodypart)] = {}
+                    
+                    # print(file_name, FRAMES, "\n", DF)
+                    try:
+                        AB = DF.loc[FRAMES[0]:FRAMES[1]]
+                        AC = DF.loc[FRAMES[0]:FRAMES[2]]
+                    except KeyError:
+                        print("Duplicate", file_name)
 
-                for bodypart in ["hindlimb", "ankle"]:
-                    Export_DFs[file_name]["AB {}".format(bodypart)]["stride_{}".format(row["Stride"])] = list(AB[bodypart])
-                    Export_DFs[file_name]["AC {}".format(bodypart)]["stride_{}".format(row["Stride"])] = list(AC[bodypart])
+                    AC = scale_dataframe(AC)
+
+                    for bodypart in LIMBS.get(SHEET):
+                        Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}".format(row["Stride"])] = list(AB[bodypart])
+                        Export_DFs[file_name]["AC_{}".format(bodypart)]["stride_{}".format(row["Stride"])] = list(AC[bodypart])
+                        Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}_angle_A".format(row["Stride"], bodypart)] = [AB.iloc[0][bodypart]] + [None for x in range(0, len(AB.index)-1)]
+                        Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}_angle_B".format(row["Stride"], bodypart)] = [AB.iloc[-1][bodypart]] + [None for x in range(0, len(AB.index)-1)]
+                        Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}_AB_midpoint".format(row["Stride"])] = [AB[bodypart].median()] + [None for x in range(0, len(AB.index)-1)]
+                        
+                        joint = None
+                        if bodypart == "forelimb":
+                            joint = "glenoid_height"
+                        elif bodypart == "hindlimb":
+                            joint = "hip_height"
+                        if joint:
+                            Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}_max_{}_height".format(row["Stride"], joint)] = [AB[joint].max()] + [None for x in range(0, len(AB.index)-1)]
+                            Export_DFs[file_name]["AB_{}".format(bodypart)]["stride_{}_min_{}_height".format(row["Stride"], joint)] = [AB[joint].min()] + [None for x in range(0, len(AB.index)-1)]
+
     
     print("\nEXPORTING")
     for file_name in Export_DFs:
         with pd.ExcelWriter(os.path.join(OUTPUT_PATH, file_name + ".xlsx")) as writer:
             sheets = Export_DFs.get(file_name)
             for sheet in sheets:
-                pd.DataFrame.from_dict(sheets.get(sheet), orient="index").transpose().to_excel(writer, sheet_name=sheet)
-    
+                pd.DataFrame.from_dict(sheets.get(sheet), orient="index").transpose().to_excel(writer, sheet_name=sheet)    
     
 
 def get_stride_frames(a, b, c):
@@ -55,7 +84,7 @@ def get_stride_frames(a, b, c):
     B = round(b * FPS)
     C = round(c * FPS)
 
-    return [A, B, C]
+    return (A, B, C)
 
 
 def scale_dataframe(DF):
@@ -66,7 +95,7 @@ def scale_dataframe(DF):
     # CREATE LIST OF DESIRED TIME POINTS
     points = [round(point * step) for point in range(0, 100)]
     points.append(TOTAL_POINTS - 1)
-    
+
     return DF.iloc[points]
 
 
@@ -77,8 +106,7 @@ if __name__ == "__main__":
 
 # TODO
 # Split to 1 sheet per body part, so for AB there will be AB hindlimb and AB ankle √
-# calculate hip and shoulder height
-# angle @ A (max AB)
-# angle @ B (min AB)
-# AB Midpoint
-# min and max hip and houlder heights
+# angle @ A √
+# angle @ B √
+# AB Midpoint (median) √
+# min and max hip and glenoid heights 
