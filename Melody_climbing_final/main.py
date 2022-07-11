e73e32740a5de1e81993cfb1be70fb65d4330c48
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2022-07-11 16:19:32 -0400

    init

--- Melody_climbing_final/main.py
+++ Melody_climbing_final/main.py
@@ -1,16 +1,29 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 import os
 
 
 BASE_DIR = os.path.dirname(__file__)
 EXCEL_PATH = os.path.join(BASE_DIR, "representative_traces.xlsx")
 
+FORCES = ["Fore/aft", "Tangential" , "Mediolateral"]
+LIMBS = ["forelimb","hindlimb"]
+LINES = {
+    "Avery" : {},
+    "Graham": {}
+}
+# COLORS
+BLUE = "#5468AD"
+YELLOW = "#F0CB58"
+
 
 def main():
-    dfs = pd.read_excel(EXCEL_PATH, sheet_name=None)
+    dfs = pd.read_excel(EXCEL_PATH, sheet_name=[0,1])
 
-    print(dfs)
+    Avery_dfs = [dfs[0].iloc[ :, np.array([1,2,3]) * (force + 1)] for force in range(len(FORCES))]
+
+    print(Avery_dfs)
 
 
 
